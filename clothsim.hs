479e232067dc86f25d131c0229599de4bd57ddb7
Manjunath Sripadarao <manju@havi.(none)>
2010-03-14 22:54:18 +0530

    Added a way to make arrays.

--- clothsim.hs
+++ clothsim.hs
@@ -1,4 +1,5 @@
 import Graphics.Rendering.OpenGL
+import Data.Array.IArray
 
 -- x::Vector3 -> Double
 x (Vector3 x y z) = x
@@ -8,7 +9,7 @@ z (Vector3 x y z) = z
 (./.) m (Vector3 x y z) = Vector3 (x/m) (y/m) (z/m) 
 
 -- Vector3 = (fromIntegral x) (fromIntegral y) (fromIntegral z) :: Vector3 GLfloat <- ???
-particles = [Vector3 x 0 z|  z <- [0.0..9.0], x <- [0.0..9.0]]
+-- particles = [Vector3 x 0 z|  z <- [0.0..9.0], x <- [0.0..9.0]]
 
 addForce::Double -> Vector3 Double -> Vector3 Double
 addForce oneOverM (Vector3 x y z) = Vector3 (x*oneOverM) (y*oneOverM) (z*oneOverM)
@@ -16,10 +17,37 @@ addForce oneOverM (Vector3 x y z) = Vector3 (x*oneOverM) (y*oneOverM) (z*oneOver
 data Particle = MkParticle { pos :: Vector3 Double, 
                              v :: Vector3 Double, 
                              f :: Vector3 Double, 
-                             oneOverM :: Double 
+                             mass :: Double 
                            } deriving (Show)
 
-data Spring = MsSpring { kd :: Float,
+data Spring = MkSpring { kd :: Float,
                          ks :: Float,
-                         r :: Float
+                         r :: Float,
+                         h :: Particle,
+                         t :: Particle
                        } deriving (Show)
+
+m = 9
+n = 9
+
+m0 = fromIntegral m ::Double
+n0 = fromIntegral n ::Double
+
+makeParticles::Double -> Double -> [Particle]
+makeParticles m0 n0 = [MkParticle { pos = Vector3  x 0.0 z, v =  Vector3 0.0 0.0 0.0, f =  Vector3 0.0 0.0 0.0, mass = 1.0 } | z <- [0.0..m0], x <- [0.0..n0]]
+
+-- particles = makeParticles (fromIntegral m) (fromIntegral n)
+
+-- a0 = array (0,9) ((0,0.0) : [(i, (fromRational i) ) | i <- [0..9], j <- [0..9]])
+particles = makeParticles m0 n0
+
+-- makeArray::(IArray Int Particle, Ix i, Num i) => i -> i -> [Particle] -> 
+makeArray :: (Num a, IArray a1 e, Particle e, Ix a) => a -> a -> [e] -> a1 a e
+makeArray m n l = listArray (0,(m+1)*n) l
+
+-- parArray0 = listArray (1,(m+1)*n) particles
+parArray0 = makeArray m n particles
+-- integrate::Float -> Array Integer Particle -> Array Integer Particle
+-- integrate dt particles = map v 
+
+-- amap f arr = listArray (bounds arr) map f (elems arr)
