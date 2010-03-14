f43ba0271b833c06c0fb7748a0fca3fcd20ac320
Manjunath Sripadarao <manju@havi.(none)>
2010-03-14 10:30:50 +0530

    1. Created types for spring and particle.

--- clothsim.hs
+++ clothsim.hs
@@ -13,9 +13,13 @@ particles = [Vector3 x 0 z|  z <- [0.0..9.0], x <- [0.0..9.0]]
 addForce::Double -> Vector3 Double -> Vector3 Double
 addForce oneOverM (Vector3 x y z) = Vector3 (x*oneOverM) (y*oneOverM) (z*oneOverM)
 
-data Particle = MkParticle { pos :: Vector3 Double, v :: Vector3 Double, f :: Vector3 Double, oneOverM :: Double } deriving (Show)
+data Particle = MkParticle { pos :: Vector3 Double, 
+                             v :: Vector3 Double, 
+                             f :: Vector3 Double, 
+                             oneOverM :: Double 
+                           } deriving (Show)
 
-[MkParticle  {pos= Vector3 x 0 z, f = Vector3 0 0 0, v = Vector3 0 0 0, oneOverM = 1/1.0} | z <- [0.0..9.0], x <- [0.0..9.0]]
--- intergate::[Particle] -> [Particle]
--- integrate source dt = map addForce 
-  
+data Spring = MsSpring { kd :: Float,
+                         ks :: Float,
+                         r :: Float
+                       } deriving (Show)
