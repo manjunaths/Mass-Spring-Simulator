fe0109cceb579c5a262493623d5c3cf2f535ab66
Manjunath Sripadarao <manjunaths@gmail.com>
2010-07-19 16:51:08 +0530

    Added trace messages for debugging the output.

--- skel.hs
+++ skel.hs
@@ -4,6 +4,7 @@ import Data.IORef ( IORef, newIORef )
 import Foreign ( newArray )
 import Vector
 import Quaternion
+import Debug.Trace
 
 data State = State { angle :: IORef GLfloat, active :: IORef Bool, x0 :: IORef GLint, y0 :: IORef GLint, dx :: IORef GLint, dy :: IORef GLint}
 
@@ -74,19 +75,18 @@ motion state (Position x y) = do
   if act == True
     then do x0acc <- get (x0 state)
             y0acc <- get (y0 state)
-            dx state $= x - x0acc
-            dy state $= y - y0acc
-            x0 state $= x
-            y0 state $= y
             dx0 <- get (dx state)
             dy0 <- get (dy state)
-            
             (_, Size width height) <- get viewport
-            let incr = update (fromIntegral width) (fromIntegral height) (fromIntegral dx0) (fromIntegral dy0) (fromIntegral x0acc) (fromIntegral y0acc)
+            let incr = update (fromIntegral width) (fromIntegral height) (fromIntegral (x - x0acc)) (fromIntegral (y0acc - y)) (fromIntegral x) (fromIntegral y)
                 -- 
-                incr_vec = getV incr
-            rotatef ((acos (getW incr) * 1.0)) (Vector3 (getX incr_vec) (getY incr_vec) (getZ incr_vec))
-
+                incr_vec = (getV incr)
+            
+            rotatef ((getW (trace( "incr = " ++ show incr) incr)) * 1.0) (Vector3 (getX incr_vec) (getY incr_vec) (getZ incr_vec))
+            dx state $= x - x0acc
+            dy state $= y0acc - y
+            x0 state $= x
+            y0 state $= y
             --translatef (Vector3 0 0 (-15))
             -- translatef (Vector3 (0.0 :: GLfloat) ( 0.0 :: GLfloat ) (0.0 :: GLfloat))
             
@@ -98,11 +98,8 @@ motion state (Position x y) = do
             y0 state $= y
             active state $= not act
             (_, Size width height) <- get viewport
-            let incr = update (fromIntegral width) (fromIntegral height) (fromIntegral 0) (fromIntegral 0) (fromIntegral x0acc) (fromIntegral y0acc)
-            --translatef (Vector3 (0.0 :: GLfloat) (0.0 :: GLfloat) (0.0 :: GLfloat))
-            -- rotatef (last incr) (Vector3 (head incr) (incr !! 1) (incr !! 2))
-            --translatef (Vector3 (0.0 :: GLfloat) (0.0 :: GLfloat) (-0.0 :: GLfloat))
-            translatef (Vector3 0 0 0)
+            let incr = update (fromIntegral width) (fromIntegral height) (fromIntegral 0) (fromIntegral 0) (fromIntegral x) (fromIntegral y)
+            rotatef 0 (Vector3 0 1 0)
             
 update::GLfloat -> GLfloat -> GLfloat -> GLfloat -> GLfloat -> GLfloat -> Quaternion
 update w h dx dy x y = incr 
@@ -111,30 +108,46 @@ update w h dx dy x y = incr
     offset = MkVec3 (w/2.0) (h/2.0) 0
     a = MkVec3 (x-dx) (h - (y+dy)) 0
     b = MkVec3  x (h - y) 0
-    a0 = (a .-. offset) ./. small
-    b0 = (b .-. offset) ./. small
+    a0 = (trace ("a = " ++ show a) a .-. trace ("offset = " ++ show offset) offset) ./. trace ("small = " ++ show small) small
+    -- a0 = (a .-. offset) ./. small
+    -- b0 = (b .-. offset) ./. small
+    b0 =  (trace ("b = " ++ show b) b .-. offset) ./. trace ("small = " ++ show small) small
     tmpscale = 1.0     
-    a1 = MkVec3 (getx a0) (gety a0) (2.0 ** ((-0.5) * (vabs a0)))
-    b1 = MkVec3 (getx b0) (gety b0) (2.0 ** ((-0.5) * (vabs b0)))
-    a2 = a1 ./. (vabs a1)
-    b2 = b1 ./. (vabs b1)
-    a_cross_b = cross a2 b2
-    axis = a_cross_b ./. (vabs a_cross_b)
+    a1 = MkVec3 (getx a0) (gety a0) (2.0 ** ((-0.5) * (vabs ((trace ("vabs a0 = " ++ show (vabs a0)) ) a0)))) -- (vabs (trace "a0 = " ++ a0 )
+    b1 = MkVec3 (getx b0) (gety b0) (2.0 ** ((-0.5) * (vabs ((trace ("vabs b0 = " ++ show (vabs b0)) ) b0)))) -- (vabs (trace "b0 = " ++ b0 ) 
+    a2 = a1 ./. trace ("vabs a1 = " ++ show (vabs a1)) (vabs a1)
+    b2 = b1 ./. trace ("vabs b1 = " ++ show (vabs b1)) (vabs b1)
+    a_cross_b = cross (trace ("vabs a2 = " ++ show (vabs a2)) a2) (trace ("vabs b2 = " ++ show (vabs b2)) b2)
+    axis = (trace ("a_cross_b = " ++ show a_cross_b) a_cross_b) ./. (vabs a_cross_b)
     angle = acos (dot a2 b2)
     -- tmp_incr = make_quat (getx axis) (gety axis) (getz axis) angle
-    tmp_incr = MkQuat axis angle 
-    incr = if dx == 0.0 && dy == 0.0 
+    -- tmp_incr = make_quat (trace ("axis = " ++ show axis) axis) (trace ("angle = " ++ show angle) angle)
+    tmp_incr = MkQuat axis angle
+    incr = if dx == 0 && dy == 0
            then MkQuat (MkVec3 0.0  1.0  0.0) 0.0
-           else tmp_incr
+           else trace ("tmp_incr = " ++ show tmp_incr) tmp_incr
                 
 
-make_quat :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> [GLfloat] 
-make_quat x y z angle = if sq_norm <= 10e-6
-                        then [0.0, 0.0, 0.0, 1.0]
-                        else [sin_theta * x, sin_theta * y, sin_theta * z, cos theta]
-                          where sq_norm = x*x + y*y + z*z
-                                theta = angle * 0.5
-                                sin_theta = sin theta
+make_quat :: Vec3 -> GLfloat -> Quaternion
+make_quat v angle = if sq_norm <= 10e-6
+                        then MkQuat (MkVec3 0.0 0.0 0.0) 1.0
+                        else MkQuat (MkVec3 (sin_theta * x) (sin_theta * y) (sin_theta * z)) (cos theta)
+                      where 
+                        x = getX v
+                        y = getY v
+                        z = getZ v
+                        sq_norm = x*x + y*y + z*z
+                        theta = angle * 0.5
+                        sin_theta = sin theta
+
+
+-- make_quat :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> [GLfloat] 
+-- make_quat x y z angle = if sq_norm <= 10e-6
+--                         then [0.0, 0.0, 0.0, 1.0]
+--                         else [sin_theta * x, sin_theta * y, sin_theta * z, cos theta]
+--                           where sq_norm = x*x + y*y + z*z
+--                                 theta = angle * 0.5
+--                                 sin_theta = sin theta
                                 
 multiply_quat :: [GLfloat] -> [GLfloat] -> [GLfloat]
 multiply_quat qr ql = qo
